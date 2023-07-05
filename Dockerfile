24615cf427b1827de6642a14136466d46c4888ea
Vinicius Pessoa <vinicius.silva3@ibm.com>
2023-07-05 00:15:45 -0300

    config build to Vercel

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:alpine
-
-WORKDIR /usr/app
-COPY ./ /usr/app
-RUN npm install
-
-EXPOSE 3000
-CMD [ "npm", "run", "dev"]
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
 import express from "express"
 import cors from "cors"
 import swaggerUi from 'swagger-ui-express';
-import swaggerFile from './swagger_output.json';
+import * as swaggerFile from './swagger_output.json';
 import endpoints from './endpoints';
 
-
 const app = express()
-const port = 3000;
+const port = process.env.port ? Number(process.env.port): 3000;
 
-// app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -17,7 +15,7 @@ app.use((req, res, next) => {
 	//Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
     res.header("Access-Control-Allow-Origin", "*");
 	//Quais são os métodos que a conexão pode realizar na API
-    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Methods", 'GET,PUT,POST');
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
